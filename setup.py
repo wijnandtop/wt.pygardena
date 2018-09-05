33e02bb6cd7d2cd93bb0436294ef243acdaeef91
Wijnand Top <w.top@4-dimensions.nl>
2018-09-05 20:45:41 +0200

    Initial version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wt.pygardena',
+    version='0.9.1',
+    packages=['pygardena'],
+    url='https://github.com/wijnandtop/wt.pygardena',
+    license='GNU General Public License v3.0',
+    author='Wijnand Top',
+    author_email='wijnand@bammes.nl',
+    description='Library to connect to Garena Smart'
+)
