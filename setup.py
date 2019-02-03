1ec6ed8f01e8b728e687f5ce07a95d46635624f9
Wijnand Top <w.top@4-dimensions.nl>
2019-02-03 14:08:18 +0100

    Feature/remove object path (#9)
    * Removed object_path dependency that occured just once
    
    * Fixed AttributeError: 'dict' object has no attribute 'properties‘

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     python_requires=">=3.4",
     install_requires=[
-        'objectpath',
         'requests',
     ],
     classifiers=[
